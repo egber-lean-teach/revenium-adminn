e3324da722f1f86018cc3d4da6f57d860b996997
egber ensignares <egber.ensignares.revenium.io>
2025-03-06 12:39:24 -0500

    fi

--- src/app/(main)/(private)/help_text/page.tsx
+++ src/app/(main)/(private)/help_text/page.tsx
@@ -7,6 +7,14 @@ export default async function DashboardView() {
   const response = await TextService.getTexts();
   const categories = await TextService.getCategories();
   const subcategories = await TextService.getSubcategories();
+
+  if (response.statusCode >= 400 || !categories || !subcategories) {
+    return (
+      <div className="w-[100vw] h-[100vh] flex justify-center items-center">
+        Error to get text, cetegories or subcategories!
+      </div>
+    );
+  }
   return (
     <ManageUser>
       <DashboardSectionOrganisms
