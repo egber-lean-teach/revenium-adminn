dacbce738f4121dba5f90c19f29fe7697cb0833d
egber ensignares <egber.ensignares.revenium.io>
2025-03-06 15:59:49 -0500

    .

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,8 +1,5 @@
 import { SignInSectionOrganisms } from "@/ui/organisms";
 
-export async function generateStaticParams() {
-  return [];
-}
 export default function SignInView() {
   return <SignInSectionOrganisms />;
 }
