2163b9600ffbe54d52320ffded43dda7222b2161
egber ensignares <egber.ensignares.revenium.io>
2025-03-06 11:32:27 -0500

    fix

--- src/app/infrastructure/utils/util.infrastructure.ts
+++ src/app/infrastructure/utils/util.infrastructure.ts
@@ -1,5 +1,5 @@
 export class UtilInfrastructure {
-  private baseUrl: string = "http://localhost:3000/api";
+  private baseUrl: string = "https://main.d1ygfq811m2mxb.amplifyapp.com/api";
 
   constructor(baseUrlClient?: string) {
     this.baseUrl = baseUrlClient || this.baseUrl;
