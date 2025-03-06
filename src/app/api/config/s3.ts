2908546dd265a2c71014703484470dc76403dda3
egber ensignares <egber.ensignares.revenium.io>
2025-03-06 09:54:58 -0500

    fix:solving problem

--- src/app/api/config/s3.ts
+++ src/app/api/config/s3.ts
@@ -1,11 +1,11 @@
 import { IS3ClientRequest } from "@/app/core/application/dto";
 
 const s3Data: IS3ClientRequest = {
-  bucketName: process.env.AWS_BUCKET_NAME || "",
-  key: process.env.AWS_KEY || "",
-  region: process.env.AWS_REGION || "",
-  secret_access_key: process.env.AWS_SECRET_ACCESS_KEY || "",
-  access_key_id: process.env.AWS_ACCESS_KEY_ID || "",
+  bucketName: process.env.BUCKET_NAME_AWS || "",
+  key: process.env.KEY_AWS || "",
+  region: process.env.REGION_AWS || "",
+  secret_access_key: process.env.SECRET_ACCESS_KEY_AWS || "",
+  access_key_id: process.env.ACCESS_KEY_ID_AWS || "",
 };
 
 export default s3Data;
