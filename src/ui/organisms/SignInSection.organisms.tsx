0a02edc6f6b15ee2afeb3f69b42a96649e9b16a7
egber-lean-teach <129096135+egber-lean-teach@users.noreply.github.com>
2025-03-07 13:28:09 -0500

    Update SignInSection.organisms.tsx

--- src/ui/organisms/SignInSection.organisms.tsx
+++ src/ui/organisms/SignInSection.organisms.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 export default function SignInSectionOrganisms(): React.ReactNode {
   return (
     <div className="flex justify-center items-center w-screen h-screen">
-      <SignIn />
+      <SignIn afterSignOutUrl={"/help_text"} />
     </div>
   );
 }
